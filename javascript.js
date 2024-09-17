379adb67619023bbe5bc61906006df713cea93b2
Kurrem <oscar.harmsen@gmail.com>
2024-09-17 16:56:07 +0200

    Code for rock-paper-scissor played in the console and with a prompt.

--- javascript.js
+++ javascript.js
@@ -1,30 +1,82 @@
 function getComputerChoice() {
-    let answer = Math.random()
+    let rand = Math.random()
 
-    if (answer < 1/3) {
+    if (rand < 1/3) {
         answer = 'rock'
-    } else if (answer >= 1/3 && answer < 2/3) {
+    } else if (rand >= 1/3 && rand < 2/3) {
         answer = 'paper'
     } else {
         answer = 'scissor'
     }
-
     return answer
 }
 
 function getHumanChoice() {
-    let answer = prompt("Please choose rock, paper or scissor.")
+    let answer = prompt("Please choose rock, paper or scissor.").toLowerCase()
 
     return answer
 }
 
-let humanScore = 0, pcScore = 0
+function playRound(humanChoice, pcChoice) {
+    let roundResult = 0             // 1 = human won. 2 = pc won. 0 = draw. 3 = error.
+    if (humanChoice === 'rock') {
+        if (pcChoice === 'scissor') {
+            roundResult = 1
+        } else if (pcChoice === 'paper') {
+            roundResult = 2
+        }
+        else {
+            roundResult = 0
+        }
+    } else if (humanChoice === 'paper') {
+        if (pcChoice === 'rock') {
+            roundResult = 1
+        } else if (pcChoice === 'scissor') {
+            roundResult = 2
+        }
+        else {
+            roundResult = 0
+        }
+    } else if (humanChoice === 'scissor') {
+        if (pcChoice === 'paper') {
+            roundResult = 1
+        } else if (pcChoice === 'rock') {
+            roundResult = 2
+        }
+        else {
+            roundResult = 0
+        }
+     } else {
+        roundResult = 3
+     }
+    return roundResult
+}
 
-let pcAnswer = getComputerChoice()
-let humanAnswer = getHumanChoice()
+function playGame() {
+    let humanScore = 0, pcScore = 0, draw = 0
 
+    for (i = 1; i < 6; i++) {
+        const humanSelection = getHumanChoice()
+        const pcSelection = getComputerChoice()
+        result = playRound(humanSelection, pcSelection)
+        
+        if (result === 1) {
+            humanScore = humanScore +1
+            console.log(humanScore)
+        } else if (result === 2) {
+            pcScore = pcScore +1
+        } else if (result === 0) {
+            draw = draw +1
+        }
+    }
 
+    if (humanScore > pcScore) {
+        console.log('Human won!')
+    } else if (pcScore > humanScore) {
+        console.log('Computer won!')
+    } else if (humanScore === pcScore) {
+        console.log("it's a draw")
+    }
+}
 
-
-
-console.log(humanScore, pcScore)
+playGame()
